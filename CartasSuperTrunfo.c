453f57ba3eb0b4faa384c4faed52ae298a5e8a17
joaogabrielns16 <joaogabrielns16@gmail.com>
2025-09-02 22:23:13 +0000

    Trabalho Pronto

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,6 +4,8 @@ int main(){
     int Número_de_pontos_turistísticos, populaçao;
     char Estado [1], Código [3], Carta, Nome_da_cidade[50];
     float Área, PIB;
+    // Eu separei em partes o codigo para uma melhor visualização e explicação sobre.
+
 
     printf("Carta 1: \n");
     printf("Estado: A \n");
@@ -12,6 +14,7 @@ int main(){
     printf("Área: 729,296km² \n");
     printf("PIB: 38.483,54R$ \n");
     printf("Número de Pontos Turísticos: 43 \n");
+    // Ambas as partes eu fiz igual, tanto em km², quanto em R$, para um melhor entendimento sobre o assunto.
     printf("Carta 2: \n");
     printf("Estado: B \n");
     printf("Código: B02 \n");
@@ -19,6 +22,8 @@ int main(){
     printf("Área: 495,977km² \n");
     printf("PIB: 54.647,38R$ \n");
     printf("Número de Pontos Turísticos: 38\n");
+    // Ja aqui no código eu usei o \n para uma melhor visualização do código, botei todas as informações em ordem iguais de ambas as cartas e fiz a separação entre a Carta 1 e Carta 2.
+
 
 
     return 0;
