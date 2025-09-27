7545f97592962cacd0f98812c5644c7ed01957a2
joaogabrielns16 <joaogabrielns16@gmail.com>
2025-09-27 16:34:49 +0000

    Metade do trabalho

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 
 int main(){
-    int Número_de_pontos_turistísticos, populaçao;
-    char Estado [1], Código [3], Carta, Nome_da_cidade[50];
-    float Área, PIB;
-    // Eu separei em partes o codigo para uma melhor visualização e explicação sobre.
+int PontosTuristicos, populacao;
+char Codigo[4], Carta, NomeCidade[50];
+char Estado1;
+float Area, PIB;
+
+printf("**Carta 1**\n");
+
+printf("Estado1: ");
+scanf("%c", &Estado1);
+
+printf("Código: ");
+scanf(" %s", Codigo);
+
+printf("Nome Da Cidade: ");
+scanf(" %s", NomeCidade);
+
+printf("População: ");
+scanf("%d", &populacao);
+
+printf("Área(em km²): ");
+scanf("%f", &Area);
+
+printf("PIB: ");
+scanf("%f", &PIB);
+
+printf("Número de Pontos Turístícos: ");
+scanf("%d", &PontosTuristicos);
+
+ printf("\nCarta 1:\n");
+    printf("Estado1: %c\n", Estado1);
+    printf("Código: %s\n", Codigo);
+    printf("Nome da Cidade: %s\n", NomeCidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", Area);
+    printf("PIB: %.2f bilhões de reais\n", PIB);
+    printf("Número de Pontos Turísticos: %d\n", PontosTuristicos);
 
 
-    printf("Carta 1: \n");
-    printf("Estado: A \n");
-    printf("Código: A01 \n");
-    printf("Nome Da Cidade: Goiânia \n");
-    printf("Área: 729,296km² \n");
-    printf("PIB: 38.483,54R$ \n");
-    printf("Número de Pontos Turísticos: 43 \n");
-    // Ambas as partes eu fiz igual, tanto em km², quanto em R$, para um melhor entendimento sobre o assunto.
-    printf("Carta 2: \n");
-    printf("Estado: B \n");
-    printf("Código: B02 \n");
-    printf("Nome Da Cidade: Porto Alegre \n");
-    printf("Área: 495,977km² \n");
-    printf("PIB: 54.647,38R$ \n");
-    printf("Número de Pontos Turísticos: 38\n");
-    // Ja aqui no código eu usei o \n para uma melhor visualização do código, botei todas as informações em ordem iguais de ambas as cartas e fiz a separação entre a Carta 1 e Carta 2.
-
-
-
-    return 0;
 
 }
